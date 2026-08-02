33a16b6eb26fd92eb60d3f3f383b8d0a7723a8c3
Sidd Ojha <138345137+smojha@users.noreply.github.com>
2026-08-02 01:18:24 -0500

    revamp personal site (incl. archiving ug content)

---
+++ ug/data/coursework-data.js
@@ -0,0 +1,240 @@
+window.COURSEWORK = {
+  "favorites": [
+    {
+      "code": "Bi 1",
+      "title": "The Great Ideas of Biology"
+    },
+    {
+      "code": "FS/Ph 11",
+      "title": "First-Year Seminar: Beyond Physics"
+    },
+    {
+      "code": "CS 21",
+      "title": "Decidability and Tractability"
+    },
+    {
+      "code": "Bi/Ge/ESE 105",
+      "title": "Evolution"
+    },
+    {
+      "code": "Ma 108a",
+      "title": "Classical Analysis"
+    },
+    {
+      "code": "BEM 114",
+      "title": "Hedge Funds"
+    },
+    {
+      "code": "Ec 117",
+      "title": "Matching Markets"
+    },
+    {
+      "code": "CMS/ACM/EE 122",
+      "title": "Mathematical Optimization"
+    },
+    {
+      "code": "CMS/CS/Ec/EE 144",
+      "title": "Networks: Structure & Economics"
+    },
+    {
+      "code": "CS/Ec 149",
+      "title": "Algorithmic Economics"
+    },
+    {
+      "code": "IDS/ACM/CS 157",
+      "title": "Statistical Inference"
+    },
+    {
+      "code": "CS/CNS/EE/IDS 159",
+      "title": "Advanced Topics in Machine Learning"
+    },
+    {
+      "code": "Ec 181a",
+      "title": "Convex Analysis and Economic Theory"
+    }
+  ],
+  "affiliations": [
+    {
+      "dept": "ACM",
+      "count": 13
+    },
+    {
+      "dept": "CS",
+      "count": 12
+    },
+    {
+      "dept": "Ec",
+      "count": 12
+    },
+    {
+      "dept": "EE",
+      "count": 12
+    },
+    {
+      "dept": "Ma",
+      "count": 9
+    },
+    {
+      "dept": "IDS",
+      "count": 8
+    },
+    {
+      "dept": "Ph",
+      "count": 8
+    },
+    {
+      "dept": "Ch",
+      "count": 3
+    },
+    {
+      "dept": "CMS",
+      "count": 3
+    },
+    {
+      "dept": "CNS",
+      "count": 3
+    },
+    {
+      "dept": "FS",
+      "count": 3
+    },
+    {
+      "dept": "H",
+      "count": 3
+    },
+    {
+      "dept": "Bi",
+      "count": 2
+    },
+    {
+      "dept": "Hum",
+      "count": 2
+    },
+    {
+      "dept": "PS",
+      "count": 2
+    },
+    {
+      "dept": "SEC",
+      "count": 2
+    },
+    {
+      "dept": "APh",
+      "count": 1
+    },
+    {
+      "dept": "BEM",
+      "count": 1
+    },
+    {
+      "dept": "ChE",
+      "count": 1
+    },
+    {
+      "dept": "En",
+      "count": 1
+    },
+    {
+      "dept": "ESE",
+      "count": 1
+    },
+    {
+      "dept": "Ge",
+      "count": 1
+    },
+    {
+      "dept": "PE",
+      "count": 1
+    },
+    {
+      "dept": "Pl",
+      "count": 1
+    },
+    {
+      "dept": "Psy",
+      "count": 1
+    },
+    {
+      "dept": "SA",
+      "count": 1
+    }
+  ],
+  "courseNumbers": [
+    105,
+    193,
+    2,
+    97,
+    106,
+    101,
+    105,
+    114,
+    87,
+    16,
+    106,
+    122,
+    135,
+    87,
+    141,
+    10,
+    12,
+    13,
+    172,
+    157,
+    181,
+    270,
+    81,
+    145,
+    108,
+    129,
+    121,
+    97,
+    21,
+    144,
+    2,
+    87,
+    108,
+    121,
+    101,
+    116,
+    117,
+    97,
+    149,
+    159,
+    114,
+    95,
+    87,
+    6,
+    3,
+    11,
+    155,
+    95,
+    87,
+    6,
+    2,
+    11,
+    122,
+    156,
+    104,
+    1,
+    1,
+    11,
+    1,
+    3,
+    1,
+    11,
+    1,
+    87,
+    1,
+    13,
+    11,
+    1,
+    1,
+    87,
+    1,
+    45,
+    1,
+    1,
+    9,
+    143
+  ]
+};
